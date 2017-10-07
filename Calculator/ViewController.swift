866d268be7dfa102036d4f562efb0a16a14fb929
muntasir14 <30627757+muntasir14@users.noreply.github.com>
2017-10-08 04:03:36 +0600

    Add files via upload

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by IKRAMUZZAMAN MUNTASIR on 8/15/17.
+//  Copyright © 2017 IKRAMUZZAMAN MUNTASIR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping {
+            
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }else {
+            
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    var displayValue: Double {
+        
+        get{
+            return Double(display.text!)!
+        }
+        set{
+            display.text = String(newValue)
+        }
+        
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        
+        userIsInTheMiddleOfTyping = false
+        if let mathemeticalSymbol = sender.currentTitle {
+            
+            switch mathemeticalSymbol {
+                
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+    
+
+}
+
